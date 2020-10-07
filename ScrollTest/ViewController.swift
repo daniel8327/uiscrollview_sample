30ed02c0df80f381055cf7902a7f84cff86ef18d
장태현 <danielchang27@gmail.com>
2020-10-07 14:35:18 +0900

    Initial Commit

---
+++ ScrollTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollTest
+//
+//  Created by moonkyoochoi on 2020/10/07.
+//  Copyright © 2020 CoreTechnologyDevelopmentTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
