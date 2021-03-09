ac4ae9f7be6a8aa0827c079a0f9884313ee1a6e2
장태현 <danielchang27@gmail.com>
2021-03-09 13:33:30 +0900

    스크롤시 네비게이션, 탭바 숨기기 기능 추가

--- ScrollTest/ViewController.swift
+++ ScrollTest/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet var scrollview: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.scrollview.delegate = self
+        
+        self.navigationItem.title = "hiroor"
+//        self.navigationController!.hidesBarsOnTap = true
+//        navigationController!.hidesBarsOnSwipe = true
     }
 
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
 
+        if(velocity.y>0) {
+            //Code will work without the animation block.I am using animation block incase if you want to set any delay to it.
+            UIView.animate(withDuration: 0.5, delay: 0, options: UIView.AnimationOptions(), animations: {
+                self.navigationController?.setNavigationBarHidden(true, animated: true)
+                self.navigationController?.setToolbarHidden(true, animated: true)
+                print("Hide")
+            }, completion: nil)
+
+        } else {
+            UIView.animate(withDuration: 0.5, delay: 0, options: UIView.AnimationOptions(), animations: {
+                self.navigationController?.setNavigationBarHidden(false, animated: true)
+                self.navigationController?.setToolbarHidden(false, animated: true)
+                print("Unhide")
+            }, completion: nil)
+          }
+       }
 }
 
